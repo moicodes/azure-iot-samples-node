72c4cc5b88c39f353ded1d5e20d7e868683aaccf
Calvin Moisan <camoisan@microsoft.com>
2020-04-23 17:17:32 -0500

    Added basic code for authenticating IoT hub connections using Key Vault. Key Vault code samples from https://medium.com/@ayanfecrown/azure-key-vault-node-js-step-by-step-tutorial-af131a78e220

--- iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
+++ iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
@@ -11,45 +11,78 @@
 // an environment variable to make it available to your application
 // or use an x509 certificate.
 // https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
-//
-// Using the Azure CLI:
-// az iot hub show-connection-string --hub-name {YourIoTHubName} --policy-name service  --output table
-var connectionString = '{Your service connection string here}';
 
-// Using the Node.js SDK for Azure Event hubs:
-//   https://github.com/Azure/azure-event-hubs-node
-// The sample connects to an IoT hub's Event Hubs-compatible endpoint
-// to read messages sent from a device.
-var { EventHubClient, EventPosition } = require('@azure/event-hubs');
+var KeyVault = require('azure-keyvault');
+var AuthenticationContext = require('adal-node').AuthenticationContext;
 
-var printError = function (err) {
-  console.log(err.message);
-};
+var clientId = "5f14da2b-4d0a-40d9-b5ea-2da07333ff30";
+var clientSecret = "18IsbjMu[W]LYt24OvZYVHbNwPbzv[A/";
+var vaultUri = "https://keyvault-cjm-test.vault.azure.net/";
 
-// Display the message content - telemetry and properties.
-// - Telemetry is sent in the message body
-// - The device can add arbitrary application properties to the message
-// - IoT Hub adds system properties, such as Device Id, to the message.
-var printMessage = function (message) {
-  console.log('Telemetry received: ');
-  console.log(JSON.stringify(message.body));
-  console.log('Application properties (set by device): ')
-  console.log(JSON.stringify(message.applicationProperties));
-  console.log('System properties (set by IoT Hub): ')
-  console.log(JSON.stringify(message.annotations));
-  console.log('');
-};
+//Authenticator - Retrieves Access token
+var authenticator = function(challenge, callback){
+      
+  //create a new auth context
+  var context = new AuthenticationContext(challenge.authorization);
 
-// Connect to the partitions on the IoT Hub's Event Hubs-compatible endpoint.
-// This example only reads messages sent after this application started.
-var ehClient;
-EventHubClient.createFromIotHubConnectionString(connectionString).then(function (client) {
-  console.log("Successfully created the EventHub Client from iothub connection string.");
-  ehClient = client;
-  return ehClient.getPartitionIds();
-}).then(function (ids) {
-  console.log("The partition ids are: ", ids);
-  return ids.map(function (id) {
-    return ehClient.receive(id, printMessage, printError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
+  //use the context to acquire an authentication token.
+  return context.acquireTokenWithClientCredentials(challenge.resource, clientId, clientSecret, function(err, tokenResponse){
+    if (err) throw err;
+
+    //calculate the value to be set in the request's Authorization header and resume the call
+    var authortizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
+
+    return callback(null, authortizationValue);
   });
-}).catch(printError);
+};
+
+var credentials = new KeyVault.KeyVaultCredentials(authenticator);
+var client = new KeyVault.KeyVaultClient(credentials);
+
+let secretName = 'jsSimDevice-servicestring';
+
+let secretVersion = ''; //leave blank to get the latest version;
+
+client.getSecret(vaultUri, secretName, secretVersion).then((result) => {
+  // Using the Azure CLI:
+  // az iot hub show-connection-string --hub-name {YourIoTHubName} --policy-name service  --output table
+  var connectionString = result.value;
+
+  // Using the Node.js SDK for Azure Event hubs:
+  //   https://github.com/Azure/azure-event-hubs-node
+  // The sample connects to an IoT hub's Event Hubs-compatible endpoint
+  // to read messages sent from a device.
+  var { EventHubClient, EventPosition } = require('@azure/event-hubs');
+
+  var printError = function (err) {
+    console.log(err.message);
+  };
+
+  // Display the message content - telemetry and properties.
+  // - Telemetry is sent in the message body
+  // - The device can add arbitrary application properties to the message
+  // - IoT Hub adds system properties, such as Device Id, to the message.
+  var printMessage = function (message) {
+    console.log('Telemetry received: ');
+    console.log(JSON.stringify(message.body));
+    console.log('Application properties (set by device): ')
+    console.log(JSON.stringify(message.applicationProperties));
+    console.log('System properties (set by IoT Hub): ')
+    console.log(JSON.stringify(message.annotations));
+    console.log('');
+  };
+
+  // Connect to the partitions on the IoT Hub's Event Hubs-compatible endpoint.
+  // This example only reads messages sent after this application started.
+  var ehClient;
+  EventHubClient.createFromIotHubConnectionString(connectionString).then(function (client) {
+    console.log("Successfully created the EventHub Client from iothub connection string.");
+    ehClient = client;
+    return ehClient.getPartitionIds();
+  }).then(function (ids) {
+    console.log("The partition ids are: ", ids);
+    return ids.map(function (id) {
+      return ehClient.receive(id, printMessage, printError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
+    });
+  }).catch(printError);
+})
--- iot-hub/Quickstarts/simulated-device/SimulatedDevice.js
+++ iot-hub/Quickstarts/simulated-device/SimulatedDevice.js
@@ -12,40 +12,77 @@
 // or use an HSM or an x509 certificate.
 // https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
 //
-// Using the Azure CLI:
-// az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
-var connectionString = '{Your device connection string here}';
 
-// Using the Node.js Device SDK for IoT Hub:
-//   https://github.com/Azure/azure-iot-sdk-node
-// The sample connects to a device-specific MQTT endpoint on your IoT Hub.
-var Mqtt = require('azure-iot-device-mqtt').Mqtt;
-var DeviceClient = require('azure-iot-device').Client
-var Message = require('azure-iot-device').Message;
+//Azure Key Vault
+//Azure Key vault implementation
+var KeyVault = require('azure-keyvault');
+var AuthenticationContext = require('adal-node').AuthenticationContext;
 
-var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+var clientId = "5f14da2b-4d0a-40d9-b5ea-2da07333ff30";
+var clientSecret = "18IsbjMu[W]LYt24OvZYVHbNwPbzv[A/";
+var vaultUri = "https://keyvault-cjm-test.vault.azure.net/";
 
-// Create a message and send it to the IoT hub every second
-setInterval(function(){
-  // Simulate telemetry.
-  var temperature = 20 + (Math.random() * 15);
-  var message = new Message(JSON.stringify({
-    temperature: temperature,
-    humidity: 60 + (Math.random() * 20)
-  }));
+//Authenticator - Retrieves Access token
+var authenticator = function(challenge, callback){
+      
+  //create a new auth context
+  var context = new AuthenticationContext(challenge.authorization);
 
-  // Add a custom application property to the message.
-  // An IoT hub can filter on these properties without access to the message body.
-  message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+  //use the context to acquire an authentication token.
+  return context.acquireTokenWithClientCredentials(challenge.resource, clientId, clientSecret, function(err, tokenResponse){
+    if (err) throw err;
 
-  console.log('Sending message: ' + message.getData());
+    //calculate the value to be set in the request's Authorization header and resume the call
+    var authortizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
 
-  // Send the message.
-  client.sendEvent(message, function (err) {
-    if (err) {
-      console.error('send error: ' + err.toString());
-    } else {
-      console.log('message sent');
-    }
+    return callback(null, authortizationValue);
   });
-}, 1000);
+};
+
+var credentials = new KeyVault.KeyVaultCredentials(authenticator);
+var client = new KeyVault.KeyVaultClient(credentials);
+
+let secretName = 'jsSimDevice-hubstring';
+
+let secretVersion = ''; //leave blank to get the latest version;
+
+client.getSecret(vaultUri, secretName, secretVersion).then((result) => {
+  // Using the Azure CLI:
+  // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+  var connectionString = result.value;
+
+  // Using the Node.js Device SDK for IoT Hub:
+  //   https://github.com/Azure/azure-iot-sdk-node
+  // The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+  var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+  var DeviceClient = require('azure-iot-device').Client
+  var Message = require('azure-iot-device').Message;
+
+  var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+
+  // Create a message and send it to the IoT hub every second
+  setInterval(function(){
+    // Simulate telemetry.
+    var temperature = 20 + (Math.random() * 15);
+    var message = new Message(JSON.stringify({
+      temperature: temperature,
+      humidity: 60 + (Math.random() * 20)
+    }));
+
+    // Add a custom application property to the message.
+    // An IoT hub can filter on these properties without access to the message body.
+    message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+
+    console.log('Sending message: ' + message.getData());
+
+    // Send the message.
+    client.sendEvent(message, function (err) {
+      if (err) {
+         console.error('send error: ' + err.toString());
+      } else {
+        console.log('message sent');
+      }
+    });
+  }, 1000);
+})
+
